f6a00a1d3f91a3750359cfc282b4f7547193ebfb
Kit <34525535+Kit-Law@users.noreply.github.com>
2022-12-06 12:15:30 +0000

    refactor(Main) Created template files for each day

---
+++ Src/Days/Day18.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <unordered_set>
+
+namespace Day18 {
+
+	inline long star1(const char* path) noexcept
+	{
+		long result = 0;
+
+		std::string buffer;
+		std::fstream input(path);
+		while (std::getline(input, buffer))
+		{
+
+		}
+
+		return result;
+	}
+
+	inline long star2(const char* path) noexcept
+	{
+		long result = 0;
+
+		std::string buffer;
+		std::fstream input(path);
+		while (std::getline(input, buffer))
+		{
+			
+		}
+
+		return result;
+	}
+
+}
